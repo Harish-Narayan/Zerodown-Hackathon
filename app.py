8048086277f5e0f2854fc8b4803b7a5da6742fec
Harish Narayan <harish83a@gmail.com>
2023-02-11 18:42:47 +0530

    complete second question with small works pending

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template,request
 from sqlalchemy import create_engine
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -12,9 +14,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Call the download_data function and get the CSV data
-    
-    download_data()
+    return render_template('index.html')
+
+
+
+
+
+@app.route('/process-form', methods=['POST'])
+def process_form():
+    print("hi")
+    input_value = request.form['id']
+    # Do something with the input value
+    print(input_value)
+    download_data(input_value)
     print("hi")
     # Load the CSV dataset into a Pandas dataframe
     df = pd.read_csv('new_file.csv')
@@ -43,7 +55,6 @@ def index():
             rating = demand_row[attribute]
             weight = weights[i]
             score += rating * weight
-            print(score)
         return score
 
     # Apply the scoring function to each demand in the dataset
@@ -52,13 +63,28 @@ def index():
     # Sort the demands by their score in descending order
     df2 = df2.sort_values(by='score', ascending=False)
     df2['score'] = (df2['score'] - df2['score'].min()) / (df2['score'].max() - df2['score'].min())
+    market_score=df2['score'].mean()
+    print(market_score)
     # Print the top-scoring demands
     print(df2.head())
-    
+    heatmap_data = df2.pivot(index='sold_homes_count', columns='median_sale_price', values='score')
+
+    # Create the heatmap
+    sns.set(font_scale=1.2)
+    sns.heatmap(heatmap_data, annot=True, fmt=".2f", cmap="YlGnBu")
+
+    # Set the axis labels
+    plt.xlabel('Median Sale Price')
+    plt.ylabel('Sold Homes Count')
+
+    # Display the heatmap
+    plt.show()
     csv2=df2.to_csv(index=False)
     df2.to_csv('final.csv',index=False)
+
     rows = [row.split(',') for row in csv2.split('\n')]
-    return render_template('index.html', csv2=rows)
+    
+    return render_template('display_input.html', input_int=market_score)
 
 
 
@@ -66,13 +92,12 @@ def index():
 
 
 
-def download_data():
+def download_data(id):
     # Connect to the PostgreSQL database using SQLAlchemy
     engine = create_engine("postgresql://postgres:admin@localhost/question2")
-    print("hi")
-    # Query the data and store it in a DataFrame
-    df = pd.read_sql("SELECT * FROM market_metrics limit 100", engine)
-    print("hi")
+    fin_id = id  # the user ID you want to select
+    fin_id=int(fin_id)
+    df = pd.read_sql("SELECT * FROM market_metrics WHERE market_id = %d  limit 10;" % fin_id, engine)
     # Save the DataFrame to a CSV file and get the CSV data as a string
     df.to_csv('new_file.csv',index=False)
 
@@ -81,7 +106,11 @@ def download_data():
 
     # Return the CSV data as a string
     return 
-
+    '''
+@app.route('/input/<int:input_int>')
+def display_input(input_int):
+    return render_template('display_input.html', input_int=input_int)
+    '''
 if __name__ == '__main__':
     app.run()
 
